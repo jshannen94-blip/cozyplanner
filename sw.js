abcf22a01e5cbb068d7c5f937ebacb797042661b
jshannen94-blip <jshannen94@gmail.com>
2026-08-12 18:53:42 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Network-first shell cache. Never intercepts API calls, always self-updates. */
-var CACHE = "today-v15";
+var CACHE = "today-v16";
 var SHELL = ["./index.html", "./calendar.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
